87834b31fd43cf4cbf7e67a4a23bee059eb7026e
sushmakurella <112331972+sushmakurella@users.noreply.github.com>
2024-01-08 11:52:29 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,16 +1,16 @@
-import streamlit as st
+timport streamlit as st
 st.set_page_config(page_title='Cats')
 st.header("Types of roses")
 col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("red roses")
-  st.image("https://4.imimg.com/data4/VI/SX/MY-36181633/red-rose.jpg")
+  st.image("https://4.imimg.com/data4/VI/SX/MY-36181633/red-rose.jpg",height=500,width=500)
   st.write("red rose is symbol for love")
 with col2:
   st.subheader("RainBow Rose")
-  st.image("https://i0.wp.com/deepgreenpermaculture.com/wp-content/uploads/2018/07/fake-rainbow-rose.jpg?resize=640%2C640&ssl=1")
+  st.image("https://i0.wp.com/deepgreenpermaculture.com/wp-content/uploads/2018/07/fake-rainbow-rose.jpg?resize=640%2C640&ssl=1",height=500,width=500)
   st.write("rainbow rose's are very beutiful")
 with col3:
   st.subheader("purple Rose")
-  st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXk74GVa0tdXcHmLUVZyGVURs4KfOR54c8gQ&usqp=CAU")
+  st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXk74GVa0tdXcHmLUVZyGVURs4KfOR54c8gQ&usqp=CAU",height=500,width=500)
   st.write("purple rose's are awesome")
