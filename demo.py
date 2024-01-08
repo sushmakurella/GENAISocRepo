c08c17fd3175877a30f754514e0e3fceb75a44fb
sushmakurella <112331972+sushmakurella@users.noreply.github.com>
2024-01-08 11:55:06 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,9 +8,9 @@ with col1:
   st.write("red rose is symbol for love")
 with col2:
   st.subheader("RainBow Rose")
-  st.image("https://i0.wp.com/deepgreenpermaculture.com/wp-content/uploads/2018/07/fake-rainbow-rose.jpg?resize=640%2C640&ssl=1")
+  st.image("https://i0.wp.com/deepgreenpermaculture.com/wp-content/uploads/2018/07/fake-rainbow-rose.jpg?resize=640%2C640&ssl=1",width=200)
   st.write("rainbow rose's are very beutiful")
 with col3:
   st.subheader("purple Rose")
-  st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXk74GVa0tdXcHmLUVZyGVURs4KfOR54c8gQ&usqp=CAU")
+  st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXk74GVa0tdXcHmLUVZyGVURs4KfOR54c8gQ&usqp=CAU",width=200)
   st.write("purple rose's are awesome")
