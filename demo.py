ee3418dd59f395d76ae027364484698a13599fc8
sushmakurella <112331972+sushmakurella@users.noreply.github.com>
2024-01-08 12:30:07 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -20,5 +20,6 @@ with col3:
 # plt.plot(data)
 # st.pyplot(plt)
 col1,col2,col3=st.columns(3)
-st.image("https://media1.tenor.com/m/5kJXpeqZbHQAAAAd/flower.gif")
+st.image("https://www.eventstodayz.com/wp-content/uploads/2020/03/rose-love-gif.gif")
+ st.subheader("cultivation of roses")
 st.video("https://www.youtube.com/watch?v=fyqttjeM8Ps&pp=ygURcm9zZXMgY3VsdGl2YXRpb24%3D")
