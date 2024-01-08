67a9b93f735860c9686c02fe7ca9e88845e32a8b
sushmakurella <112331972+sushmakurella@users.noreply.github.com>
2024-01-08 11:47:58 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 import streamlit as st
 st.set_page_config(page_title='Cats')
-st.header("types od cats")
+st.header("Types of roses")
 col1,col2=st.columns(2)
 with col1:
-  st.subheader("Persian cat")
-  st.image("https://static.toiimg.com/thumb/msid-98820330,width-960,height-1280,resizemode-6.cms")
-  st.write("persian cats aare cute")
+  st.subheader("red roses")
+  st.image("https://4.imimg.com/data4/VI/SX/MY-36181633/red-rose.jpg")
+  st.write("red rose is symbol for love")
 with col2:
   st.subheader("RainBow Rose")
   st.image("https://i0.wp.com/deepgreenpermaculture.com/wp-content/uploads/2018/07/fake-rainbow-rose.jpg?resize=640%2C640&ssl=1")
