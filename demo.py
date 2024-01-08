fdcb0ecc8dd2da4783c1becf2effd2ad6a21294f
sushmakurella <112331972+sushmakurella@users.noreply.github.com>
2024-01-08 11:54:22 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ st.header("Types of roses")
 col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("red roses")
-  st.image("https://4.imimg.com/data4/VI/SX/MY-36181633/red-rose.jpg")
+  st.image("https://4.imimg.com/data4/VI/SX/MY-36181633/red-rose.jpg",width=200)
   st.write("red rose is symbol for love")
 with col2:
   st.subheader("RainBow Rose")
