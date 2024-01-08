80147d6ee4fb1dd1142f2d35baf4fe4207a74485
sushmakurella <112331972+sushmakurella@users.noreply.github.com>
2024-01-08 11:45:06 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,8 +5,8 @@ col1,col2=st.columns(2)
 with col1:
   st.subheader("Persian cat")
   st.image("https://static.toiimg.com/thumb/msid-98820330,width-960,height-1280,resizemode-6.cms")
-  st.witre("persian cats aare cute")
+  st.write("persian cats aare cute")
 with col2:
   st.subheader("RainBow Rose")
   st.image("https://i0.wp.com/deepgreenpermaculture.com/wp-content/uploads/2018/07/fake-rainbow-rose.jpg?resize=640%2C640&ssl=1")
-  st.witre("rainbow rose's are very beutiful")
+  st.write("rainbow rose's are very beutiful")
